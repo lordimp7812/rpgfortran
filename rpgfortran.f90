e062bb78318ced0dee2302ffd9f48c7854ce0ae4
lord <souza7812@gmail.com>
2020-09-13 10:28:35 -0300

    initial change

--- rpgfortran.f90
+++ rpgfortran.f90
@@ -1,20 +1,68 @@
 
 
 
-program main
+program rpg
    implicit none
-   !integer ::
-   !real ::
-   write(*,*)
-   write(*,*) 'Objetivo do programa: '
-   write(*,*)
-   write(*,*) 'Autor: '
-   write(*,*) 'Matricula: '
-   write(*,*) 'Data do programa: '
-   write(*,*) 'Ultima modificacao: '
-   write(*,*)
+   integer :: a, V ,i
+ real :: x
+V=20
 
-   ! insira seus comandos
-   ! aqui
 
-end program main
+   
+
+
+
+
+
+
+
+write(*,*) "Digite 1 para receber ajuda ou digite 2 para iniciar a aventura"
+read (*,*) a
+select case (a)
+case(1)
+Write(*,*) "Esse rpg e baseado em tomar decisoes" 
+Write(*,*) "escolha bem para seguir a aventura o maximo"
+Write(*,*) "boa sorte"
+write(*,*)"A aventura comeca..."
+case(2)
+write(*,*)" A ventura comeca..."
+end select
+ 
+ 
+ 
+ 
+
+Write(*,*) "voce acorda em sua cama e sente que um tremor" 
+Write(*,*) "escolha:" 
+Write(*,*) "Digite 1 para sair da casa para ver"
+Write(*,*) "Digite 2 para voltar a dormir"
+write(*,*)  "Digite 3 para permanecer na casa"
+
+read (*,*) a
+selectcase(a)
+case(1)
+ Write(*,*) "alguma coisa acontece"
+case (3)
+ write (*,*) "voce fica preso na casa"
+
+case(2)
+ Write(*,*) "fim do jogo" 
+end select
+
+ write (*,*) "ache uma saida ou morra de fome"
+write (*,*) "sua barra de vida 20"
+Write (*,*) ""
+
+
+
+
+
+
+
+
+
+end program rpg
+   
+   
+
+
