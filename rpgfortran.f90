5510b226a2a3b71ff2f7ef337a475b695d810ce8
lord <souza7812@gmail.com>
2020-09-13 10:18:22 -0300

    initial commit

---
+++ rpgfortran.f90
@@ -0,0 +1,20 @@
+
+
+
+program main
+   implicit none
+   !integer ::
+   !real ::
+   write(*,*)
+   write(*,*) 'Objetivo do programa: '
+   write(*,*)
+   write(*,*) 'Autor: '
+   write(*,*) 'Matricula: '
+   write(*,*) 'Data do programa: '
+   write(*,*) 'Ultima modificacao: '
+   write(*,*)
+
+   ! insira seus comandos
+   ! aqui
+
+end program main
